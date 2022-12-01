d4903425a67510aa9021166feeac1846b717b016
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2022-12-01 17:06:54 -0300

    initial commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,15 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  const route = useRouter();
+  return (
+    <Link href="/">
+      <header className={styles.header}>
+        <Image width={240} height={25} src="/logo.svg" alt="Logo" />
+      </header>
+    </Link>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,27 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <title>Blog</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
